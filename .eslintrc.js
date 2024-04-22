5e732c72c1c5c2cff9cfeb16d36dd5ca0b76a04b
Christian CoC <christianelmillo81@gmail.com>
2024-04-22 08:58:02 -0300

    agregando archivos faltantes

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+
+window.eslintConfig = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {},
+};
